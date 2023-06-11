7269d938abc652eddd6ac7b4c820b982b75e23b8
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2023-06-12 00:47:14 +0900

    🐛 SWC revert..

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const { withContentlayer } = require('next-contentlayer');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true, // Terser가 아닌 SWC
+  // swcMinify: true, // Terser가 아닌 SWC
   reactStrictMode: true,
   i18n: {
     locales: ['ko'],
