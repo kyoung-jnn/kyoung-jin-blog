4be445d7876d82b941bbba7080398c82808c4b89
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2023-07-10 01:28:12 +0900

    🔧 SWC 적용 및 린트 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,13 +16,15 @@ module.exports = {
   parserOptions: {
     ecmaFeatures: { jsx: true },
   },
-  plugins: ['@typescript-eslint', 'import', 'react', '@emotion'],
   extends: [
     'eslint:recommended',
-    'next',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@next/next/recommended',
     'prettier',
   ],
+  plugins: ['@typescript-eslint', 'import', 'react', 'react-hooks', '@emotion'],
   settings: {
     'import/resolver': { typescript: {} },
     react: { version: 'detect' },
@@ -30,5 +32,6 @@ module.exports = {
   rules: {
     'react/react-in-jsx-scope': STATUS.OFF, // import React 필수
     'react/no-unknown-property': [STATUS.ERROR, { ignore: ['css'] }], // emotion css attributes
+    'react-hooks/exhaustive-deps': STATUS.ERROR,
   },
 };
