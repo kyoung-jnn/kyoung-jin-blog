2c4e800c25d7e09e9795d879910ca4b953417a93
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2023-10-03 02:12:43 +0900

    Revert "✨ 아이콘 대소문자 수정"
    This reverts commit 1868871e5ab2970a3123a899ecabc3709d855dd9.

---
+++ components/icons/menu.tsx
@@ -0,0 +1,16 @@
+export default function Menu({ className }: { className?: string }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 20 20"
+      className={className}
+    >
+      <path
+        fillRule="evenodd"
+        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+        clipRule="evenodd"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
