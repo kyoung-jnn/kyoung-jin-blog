06cdf59a7b8ad799711fe2d2f60b2d2e998854d9
kyoung-jnn <55469709+kyoung-jnn@users.noreply.github.com>
2024-09-21 01:57:42 +0900

    ✨ gallery

--- database/gallery.ts
+++ database/gallery.ts
@@ -2,11 +2,12 @@ import image_20240209 from '@/public/gallery/20240209.jpg';
 import image_20240301_1 from '@/public/gallery/20240301_1.jpeg';
 import image_20240301_2 from '@/public/gallery/20240301_2.jpeg';
 import image_20240303_1 from '@/public/gallery/20240303_1.jpeg';
+import image_20240803 from '@/public/gallery/20240803.jpg';
 
 export type GalleryItemType = 'image' | 'video';
 
 const GALLERY_LIST = [
-  { type: 'image', src: image_20240209, alt: '첫 합주 2024.02.' },
+  { type: 'image', src: image_20240803, alt: '펜타포트 락 페스티벌 2024.08.' },
   {
     type: 'image',
     src: image_20240303_1,
@@ -32,6 +33,7 @@ const GALLERY_LIST = [
     src: '/gallery/20240303_1.mp4',
     alt: '제주, 맑음 2024.03.',
   },
+  { type: 'image', src: image_20240209, alt: '첫 합주 2024.02.' },
 ] as const;
 
 export default GALLERY_LIST;
